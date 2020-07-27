1e0cc5d2c227b04dc79ce6c0bb1a4bf98e475f28
MD-ietz <43513494+MD-ietz@users.noreply.github.com>
2020-07-27 23:43:23 +0200

    Shellcheck and names in functions.sh

--- functions.sh
+++ functions.sh
@@ -2,24 +2,23 @@
 dotfilesDir=$(pwd)
 
 function linkDotfile {
-  dest="${HOME}/${1}"
-  dateStr=$(date +%Y-%m-%d-%H%M)
+  dotFile="${HOME}/${1}"
+  dateString=$(date +%Y-%m-%d-%H%M)
 
-  [ -h ~/${1} ] && rm ${dest}
-  [ -f "${dest}" ] && mv ${dest}{,.${dateStr}}
-  [ -d "${dest}" ] && mv ${dest}{,.${dateStr}}
+  [ -h "${dotFile}" ] && rm "${dotFile}"
+  [ -f "${dotFile}" ] && mv "${dotFile}"{,."${dateString}"}
+  [ -d "${dotFile}" ] && mv "${dotFile}"{,."${dateString}"}
 
-  echo "Creating symlink: ${dest}"
-  ln -s ${dotfilesDir}/${1} ${dest}
+  echo "Creating symlink: ${dotFile}"
+  ln -s "${dotfilesDir}/${1}" "${dotFile}"
 }
 
 function install {
   local program="${1}"
   if [ ! "$(which "${program}")" ]; then
-    echo "Installing: ${1}..."
-    sudo apt-get install "${1}"
+    echo "Installing: ${program}..."
+    sudo apt-get install "${program}"
   else
-    echo "Already installed: ${1}"
+    echo "Already installed: ${program}"
   fi
 }
-
