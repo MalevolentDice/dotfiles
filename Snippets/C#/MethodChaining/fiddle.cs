c2708f5fde5107054b5ce53e6dd24b0355693c92
MalevolentDice <MalevolentDice@users.noreply.github.com>
2021-12-02 16:35:06 +0100

    Added MethodChaining snippet

---
+++ Snippets/C#/MethodChaining/fiddle.cs
@@ -0,0 +1,64 @@
+using System;
+public interface IName
+{
+    IAge WithName(string name);
+}
+
+public interface IAge
+{
+    IPersist WithAge(int age);
+}
+
+public interface IPersist
+{
+    void Save();
+}
+
+public class Person : IName, IAge, IPersist
+{
+
+  public string Name { get; private set; }
+    public int Age { get; private set; }
+
+    private Person()
+  {    
+  }
+
+  public static IName Create()
+  {
+    return new Person();
+  }
+
+    public IAge WithName(string name)
+    {
+        Name = name;
+        return this;
+    }
+
+    public IPersist WithAge(int age)
+    {
+        Age = age;
+        return this;
+    }
+
+    public void Save()
+    {
+        // save changes here
+    Console.WriteLine("Name is :"+Name);
+    Console.WriteLine("Age is :"+Age);
+    Console.WriteLine("Save Called");
+    }
+}
+
+public class Program
+{
+  public static void Main(string[] args)
+  {
+    Person
+      .Create()
+      .WithName("Venkat")
+      .WithAge(25)
+      .Save();
+    
+  }
+}
