45b594c20717794e90ab3fd84b8abafac5dc33ad
MalevolentDice <MalevolentDice@users.noreply.github.com>
2023-06-16 19:12:40 +0200

    Formatting in C# Snippet

--- Snippets/C#/MethodChaining/fiddle.cs
+++ Snippets/C#/MethodChaining/fiddle.cs
@@ -1,53 +1,49 @@
 using System;
 public interface IName
 {
-    IAge WithName(string name);
+  IAge WithName(string name);
 }
 
 public interface IAge
 {
-    IPersist WithAge(int age);
+  IPersist WithAge(int age);
 }
 
 public interface IPersist
 {
-    void Save();
+  void Save();
 }
 
 public class Person : IName, IAge, IPersist
 {
-
   public string Name { get; private set; }
-    public int Age { get; private set; }
+  public int Age { get; private set; }
 
-    private Person()
-  {    
-  }
+  private Person()  {}
 
   public static IName Create()
   {
     return new Person();
   }
 
-    public IAge WithName(string name)
-    {
-        Name = name;
-        return this;
-    }
+  public IAge WithName(string name)
+  {
+    Name = name;
+    return this;
+  }
 
-    public IPersist WithAge(int age)
-    {
-        Age = age;
-        return this;
-    }
+  public IPersist WithAge(int age)
+  {
+    Age = age;
+    return this;
+  }
 
-    public void Save()
-    {
-        // save changes here
+  public void Save()
+  {
     Console.WriteLine("Name is :"+Name);
     Console.WriteLine("Age is :"+Age);
     Console.WriteLine("Save Called");
-    }
+  }
 }
 
 public class Program
