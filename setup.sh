0e6cc3b77ee26e350ef709ca897d72faf22db33a
MalevolentDice <MalevolentDice@users.noreply.github.com>
2024-10-02 23:45:57 +0200

    Vulkan i386

--- setup.sh
+++ setup.sh
@@ -45,7 +45,7 @@ sudo add-apt-repository ppa:oibaf/graphics-drivers && sudo apt update && sudo ap
 echo "deb [signed-by=/etc/apt/keyrings/lutris.gpg] https://download.opensuse.org/repositories/home:/strycore/Debian_12/ ./" | sudo tee /etc/apt/sources.list.d/lutris.list > /dev/null
 wget -q -O- https://download.opensuse.org/repositories/home:/strycore/Debian_12/Release.key | gpg --dearmor | sudo tee /etc/apt/keyrings/lutris.gpg > /dev/null
 sudo apt update
-sudo apt install libvulkan1 mesa-vulkan-drivers vulkan-utils
+sudo apt install libvulkan1:i386 mesa-vulkan-drivers:i386 libvulkan1 mesa-vulkan-drivers vulkan-utils
 sudo apt install lutris
 
 read -p "Press Enter to reboot"
